a8e5e1574c07f9a701c415ae0c58dcfc140ccfb5
刘展豪 <3101613795@qq.com>
2025-08-21 14:38:35 +0800

    前端部署

--- api/callback.js
+++ api/callback.js
@@ -67,8 +67,8 @@ export default async function handler(req, res) {
     ]);
 
     // 重定向前端成功页
-    res.redirect("http://localhost:5173/callback-success");
-    // res.redirect(`${process.env.FRONTEND_URL}/callback-success`);
+    // res.redirect("http://localhost:5173/callback-success");
+    res.redirect(`${process.env.FRONTEND_URL}/callback-success`);
   } catch (err) {
     console.error("Callback error:", err.response?.data || err.message);
     res.status(500).json({
