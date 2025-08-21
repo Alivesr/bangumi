1620c56997da6d50024c88ed81602b86a0d478c7
刘展豪 <3101613795@qq.com>
2025-08-21 11:24:09 +0800

    callback-succsess页面

---
+++ src/page/home/callback-success/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+onMounted(() => {
+  console.log("Callback Success");
+});
+</script>
+
+<template>
+  <div>
+    <h1>Callback Success</h1>
+    <p>登录成功</p>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,8 @@ import Subject_Search from "@/page/home/subject_search/index.vue";
 import home from "@/page/home/home-p/index.vue";
 import Subject from "@/page/home/subject/index.vue";
 import User from "@/page/home/user/index.vue";
+// @ts-ignore
+import CallbackSuccess from "@/page/home/callback-success/index.vue";
 const routes = [
   {
     path: "/",
@@ -36,6 +38,11 @@ const routes = [
         path: "user",
         name: "User",
         component: User,
+      },
+      {
+        path: "callback-success",
+        name: "CallbackSuccess",
+        component: CallbackSuccess,
       },
     ],
   },
